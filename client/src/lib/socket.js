0d384d1ba281799b699f3e90d26ad7870cd285d5
Aman Mondal <162113539+konlin008@users.noreply.github.com>
2026-05-20 12:50:36 +0530

    update socket.io client url to production server

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080", {
+const socket = io("https://team-task-manager-m25e.onrender.com", {
   withCredentials: true,
 });
 
